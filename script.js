5d1647d46c575aff4c2a08bd3035183cbd73439c
Iris Coats <iris.coats73@gmail.com>
2024-01-23 22:13:22 -0600

    code entered

--- script.js
+++ script.js
@@ -17,8 +17,29 @@ function displayPlants(){
         const li=document.createElement("li")
 
     li.textContent= Name: ${plant.name},
-    species: ${plant.species}, water_schedule: ${plant.water_schedule};
+    species: ${plant.species}, water_schedule: ${plant.water};
     plantList.appendChild(li);
 
     }
 };
+
+function addPlant(name, species, water){
+    const newPlant = { name, species, water};
+    plant.push(newPlant);
+}
+
+Form.addEventListener("submit", addPlantFromForm)
+
+function addBookFromForm(event){
+    event.preventDefault();
+}
+
+const Name=form.Name.value;
+const species=form.species.value;
+const water=form.water.value;
+
+addPlant(Name, species, water);
+form.reset();
+
+addPlant(Name, species, water);
+displayPlants();
